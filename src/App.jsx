c105f526344164940894b9203ba8015997cdd89a
safayet <safayetdib@gmail.com>
2023-02-02 18:37:51 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+	<div className="w-full overflow-hidden bg-primary">
+		<h1 className="text-7xl text-white">Hello World</h1>
+	</div>
+);
+
+export default App;
